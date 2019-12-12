ba7ed3d5b35a6b99f8fdaf3f86eaa475ad69f9e0
vladimirKartyshov <vovan198080@inbox.ru>
2019-12-12 14:26:54 +0300

    Implementashion of behavior 2

--- src/main/java/com/example/timer/MainActivity.java
+++ src/main/java/com/example/timer/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.timer;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -26,26 +29,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
 
-                int minutes = progress/60;
-                int seconds = progress - (minutes*60);
-
-                //если число на таймере будет однозначным, и чтобы не изменился интерфейс
-                String minutesString = "";
-                String secondsString = "";
-
-                if (minutes < 10){
-                    minutesString = "0" + minutes;
-                }else {
-                    minutesString = String.valueOf(minutes);
-                }
-
-                if (seconds < 10){
-                    secondsString = "0" + seconds;
-                }else {
-                    secondsString = String.valueOf(seconds);
-                }
-                //устанавливаем в textView наши значения
-                textView.setText(minutesString + ":" + secondsString);
+              long progressInMillis = progress * 1000;
+              updateTimer(progressInMillis);
             }
 
             @Override
@@ -58,6 +43,47 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
 
+    public void start(View view) {
+
+        CountDownTimer countDownTimer = new CountDownTimer(seekbar.getProgress() * 1000,1000){
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            updateTimer( millisUntilFinished);
+
+            }
+
+            @Override
+            public void onFinish() {
+
+                Log.d("onFinish", "Finish!");
+            }
+        }.start();
+    }
+
+    private void updateTimer(long millisUntilFinished){
+
+        int minutes = (int)millisUntilFinished/1000/60;
+        int seconds = (int)millisUntilFinished/1000 - (minutes*60);
+
+        //если число на таймере будет однозначным, и чтобы не изменился интерфейс
+        String minutesString = "";
+        String secondsString = "";
+
+        if (minutes < 10){
+            minutesString = "0" + minutes;
+        }else {
+            minutesString = String.valueOf(minutes);
+        }
+
+        if (seconds < 10){
+            secondsString = "0" + seconds;
+        }else {
+            secondsString = String.valueOf(seconds);
+        }
+        //устанавливаем в textView наши значения
+        textView.setText(minutesString + ":" + secondsString);
     }
 }
