537386eb5f0bcf88c4835a9d8c95ff1f27454cfb
vladimirKartyshov <vovan198080@inbox.ru>
2019-12-18 16:44:02 +0300

    Settings Activity has been completed

---
+++ src/main/java/com/example/timer/AboutActivity.java
@@ -0,0 +1,34 @@
+package com.example.timer;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        //переопределяем поведение кнопки стрелки в меню, чтобы возвращаться в Main.Activity
+        ActionBar actionBar = this.getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        // этот код при нажатии на кнопку-стрелку возвращает в родительскую активити
+        if (id == android.R.id.home){
+            NavUtils.navigateUpFromSameTask(this);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- src/main/java/com/example/timer/MainActivity.java
+++ src/main/java/com/example/timer/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.timer;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -109,5 +114,28 @@ public class MainActivity extends AppCompatActivity {
         seekbar.setEnabled(true);
         seekbar.setProgress(30);
         isTimerOn = false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //создаем(надуваем) меню в main.activity из xml файла timer_menu.xml
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.timer_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();//получаем эл-т, который выбран в меню
+        if (id == R.id.action_settings){
+            Intent openSetting = new Intent(this,SettingsActivity.class);
+            startActivity(openSetting);
+            return true;
+        }else  if (id == R.id.action_about){
+            Intent openAbout = new Intent(this, AboutActivity.class);
+            startActivity(openAbout);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
