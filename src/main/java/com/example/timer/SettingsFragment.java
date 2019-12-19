5f9c9442d7ddaa92ef384c7c1ea00d523d53686a
vladimirKartyshov <vovan198080@inbox.ru>
2019-12-19 18:54:02 +0300

    SettingsFragment

---
+++ src/main/java/com/example/timer/SettingsFragment.java
@@ -0,0 +1,14 @@
+package com.example.timer;
+
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+
+    }
+}
+          // НЕ РЕАЛИЗОВАНО МЕНЮ НАСТРОЕК ПОЛЬЗОВАТЕЛЯ
